60f1410af4bac73645b83ef4ddf96c588cc0be95
sanjay <113435192+sanjay-lgtm@users.noreply.github.com>
2024-01-29 19:23:08 +0530

    craete signup api route

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRoutes from './routes/user.route.js';
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose
@@ -14,9 +16,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
